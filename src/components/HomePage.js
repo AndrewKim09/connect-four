3c087102216ce9e40f224dc784c2544a370e2111
Andrew Kim <andrewkim09@hotmail.com>
2024-01-31 12:47:53 -0500

    4th commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,12 +13,12 @@ export const HomePage = () => {
   }
   return (
     <div className=" w-[480px] h-[435px] border-black border-[3px] m-auto rounded-[10%] flex flex-col shadow-[0_10px_0_0_rgba(0,0,0)]">
-        <img src='../assets/images/logo.svg' alt = "logo" className="w-[52px] h-[52px] m-auto mt-[70px]"></img>
+        <div alt = "logo" className="homeLogo image w-[52px] h-[52px] m-auto mt-[70px]"/>
 
         <div role="buttonWraper" className="flex justify-center w-[407px] h-[85px] pb-[10px] pt-[3px] rounded-2xl items-center mx-[40px] bg-black">
           <button className="flex justify-between w-[400px] h-[72px] bg-yellowPlayer rounded-xl px-[20px] items-center" onClick={() => {onPlayVsPlayer()}}>
               <span className="text-HeadingMd">PLAY VS PLAYER</span>
-              <img src='../assets/images/player-vs-player.svg' alt = "player-vs-player" className="w-[82px] h-[34px]"></img>
+              <div className="playVsPlayerImage h-[30px] w-[30px] image" alt="playVsPlayerImage"/>
           </button>
         </div>
 
