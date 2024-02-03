71f19aec6f550b37cc08c8848549b8fccd2389b4
Andrew Kim <andrewkim09@hotmail.com>
2024-02-03 17:20:55 -0500

    6th commit

--- src/components/PlayVsPlayer.js
+++ src/components/PlayVsPlayer.js
@@ -3,12 +3,56 @@ import $ from 'jquery'
 
 export const PlayVsPlayer = () => {
   var redTurn = true;
-  const [playerTurnText, setPlayerTurnText] = useState("PLAYER 1'S TURN");
+  const [playerTurnText, setPlayerTurnText] = useState("PLAYER 1'S TURN");// w-[3.78vh] aspect-square rounded-full border-4 border-solid border-white bg-transparent m-auto mt-[1.67vh]
   var lastMove;
   var timePassed = 0;
-  const [timer, setTimer] = useState(15);
+  var winner = false;
+  var nextPlayer;
+  const [timer, setTimer] = useState("15");
   const MAXTIME = 15;
   var piecesPlayed = 0;
+
+  const findWinningPieces = (pieces, bool) => {
+    console.log("finding winning pieces")
+    for(let i = 0; i < pieces.length - 3; i++){
+      if(pieces.slice(i, i+4).every((piece) => piece.hasClass(redTurn ? "red" : "yellow") == redTurn == bool)){
+        for(let j = i; j < i+4; j++){
+          pieces[j].children().addClass("winningPiece")
+        }
+        break;
+      }
+    }
+
+  }
+
+
+  const resetBoard = () => {
+    $(".pieces").children().each(function() {
+      $(this).removeClass("red")
+      $(this).removeClass("yellow")
+      $(this).addClass("empty")
+      $(this).children().removeClass("winningPiece")
+    });
+
+    $(".bottomColor").removeClass("bottomUpAnimation")
+      $(".turn").removeClass("bottomUpAnimation")
+    
+    $(".playAgainBox").addClass("hidden");
+    $(".bottomColor").removeClass("bg-redPlayer")
+    $(".bottomColor").removeClass("bg-yellowPlayer")
+    $(".bottomColor").addClass("bg-darkPurple")
+    $(".turn").removeClass("winningBox")
+    $(".playerText").removeClass("mt-[1.9vh]");
+      $(".playerText").addClass("mt-[20%]");
+    if(nextPlayer == "PLAYER 1"){
+      redTurn = false;
+
+    }
+    else{
+      redTurn = true;
+    } 
+    winner = false
+  }
 
   const checkWin = () => {
     console.log("checking for win");
@@ -24,19 +68,20 @@ export const PlayVsPlayer = () => {
 
     //get horizontal and vertical
     for(let i = 1; i < 8; i++){
-      horizontalPieces.push($(".row-"+lastMove.row+".col-"+i).hasClass(redTurn ? "red" : "yellow") == redTurn);
+      horizontalPieces.push($(".row-"+lastMove.row+".col-"+i));
       if(i < 7)
-        verticalPieces.push($(".row-"+i+".col-"+lastMove.column).hasClass(redTurn ? "red" : "yellow") == redTurn);
+        verticalPieces.push($(".row-"+i+".col-"+lastMove.column));
     }
 
     //get top left to bottom right
     var count = 0;
     while($(".row-"+(parseInt(lastMove.row)+count)+".col-"+(parseInt(lastMove.column)-count)).length > 0){
-      diagonalPieces.push($(".row-"+(parseInt(lastMove.row)+count)+".col-"+(parseInt(lastMove.column)-count)).hasClass(redTurn ? "red" : "yellow") == redTurn);
+      diagonalPieces.unshift($(".row-"+(parseInt(lastMove.row)+count)+".col-"+(parseInt(lastMove.column)-count)));
       count++;
     }
+    count = 1;
     while($(".row-"+(parseInt(lastMove.row)-count)+".col-"+(parseInt(lastMove.column)+count)).length > 0){
-      diagonalPieces.push($(".row-"+(parseInt(lastMove.row)-count)+".col-"+(parseInt(lastMove.column)+count)).hasClass(redTurn ? "red" : "yellow") == redTurn);
+      diagonalPieces.push($(".row-"+(parseInt(lastMove.row)-count)+".col-"+(parseInt(lastMove.column)+count)));
       count++;
     }
 
@@ -44,11 +89,12 @@ export const PlayVsPlayer = () => {
 
     //get bottom left to top right
     while($(".row-"+(parseInt(lastMove.row)-count)+".col-"+(parseInt(lastMove.column)-count)).length > 0){
-      secondDiagonalPieces.push($(".row-"+(parseInt(lastMove.row)-count)+".col-"+(parseInt(lastMove.column)-count)).hasClass(redTurn ? "red" : "yellow") == redTurn);
+      secondDiagonalPieces.unshift($(".row-"+(parseInt(lastMove.row)-count)+".col-"+(parseInt(lastMove.column)-count)));
       count++;
     }
+    count = 1;
     while($(".row-"+(parseInt(lastMove.row)+count)+".col-"+(parseInt(lastMove.column)+count)).length > 0){
-      secondDiagonalPieces.push($(".row-"+(parseInt(lastMove.row)+count)+".col-"+(parseInt(lastMove.column)+count)).hasClass(redTurn ? "red" : "yellow") == redTurn);
+      secondDiagonalPieces.push($(".row-"+(parseInt(lastMove.row)+count)+".col-"+(parseInt(lastMove.column)+count)));
       count++;
     }
 
@@ -57,39 +103,93 @@ export const PlayVsPlayer = () => {
     console.log(diagonalPieces);
     console.log(secondDiagonalPieces);
 
-    for(let i = 0; i <= 7; i++){
-      horizontalString += horizontalPieces[i];
-      if(i < 7)
-        verticalString += verticalPieces[i]
+    for(let i = 0; i < 7; i++){
+      horizontalString += horizontalPieces[i].hasClass(redTurn ? "red" : "yellow") == redTurn;
+      if(i < 6)
+        verticalString += verticalPieces[i].hasClass(redTurn ? "red" : "yellow") == redTurn;
     }
 
     for(let i = 0; i < diagonalPieces.length; i++){
-      diagonalString += diagonalPieces[i];
-      secondDiagonalString += secondDiagonalPieces[i];
+      diagonalString += diagonalPieces[i].hasClass(redTurn ? "red" : "yellow") == redTurn;
     }
+    for(let i = 0; i < secondDiagonalPieces.length; i++){
+      secondDiagonalString += secondDiagonalPieces[i].hasClass(redTurn ? "red" : "yellow") == redTurn;
+    }
+
+
 
 
     if(redTurn){
       if(horizontalString.includes("true".repeat(4)) || verticalString.includes("true".repeat(4)) || diagonalString.includes("true".repeat(4)) || secondDiagonalString.includes("true".repeat(4))){
         console.log("WINNER")
-        if(redTurn){
-          setPlayerTurnText("PLAYER 1 WINS")
+        setPlayerTurnText("PLAYER 1")
+        setTimer("WINS");
+        $(".bottomColor").removeClass("bg-darkPurple")
+        $(".bottomColor").addClass("bg-redPlayer")
+
+        if(horizontalString.includes("true".repeat(4))){
+          findWinningPieces(horizontalPieces, true)
         }
-        else{
-          setPlayerTurnText("PLAYER 2 WINS")
+        else if(verticalString.includes("true".repeat(4))){
+          findWinningPieces(verticalPieces, true)
         }
+
+        else if(diagonalString.includes("true".repeat(4))){
+          findWinningPieces(diagonalPieces, true);
+        }
+
+        else if(secondDiagonalString.includes("true".repeat(4))){
+          findWinningPieces(secondDiagonalPieces, true)
+        }
+
+        winner = true;
       }
     }
 
     else{
       if(horizontalString.includes("false".repeat(4)) || verticalString.includes("false".repeat(4)) || diagonalString.includes("false".repeat(4)) || secondDiagonalString.includes("false".repeat(4))){
         console.log("WINNER")
-        if(redTurn){
-          setPlayerTurnText("PLAYER 1 WINS")
-        }
-        else{
-          setPlayerTurnText("PLAYER 2 WINS")
-        }
+          setPlayerTurnText("PLAYER 2")
+          setTimer("WINS");
+          $(".bottomColor").removeClass("bg-darkPurple")
+          $(".bottomColor").addClass("bg-yellowPlayer")
+          winner = true;
+
+          if(horizontalString.includes("false".repeat(4))){
+            findWinningPieces(horizontalPieces, false, "horizontal");
+          }
+
+          else if(verticalString.includes("false".repeat(4))){
+            findWinningPieces(verticalPieces, false, "vertical");
+          }
+            
+          else if(diagonalString.includes("false".repeat(4))){
+            findWinningPieces(diagonalPieces, false, "diagonal");
+          }
+  
+          else if(secondDiagonalString.includes("false".repeat(4))){
+            findWinningPieces(secondDiagonalPieces, false, "secondDiagonal");
+          }
+
+      }
+    }
+
+    if(winner){
+
+      $(".bottomColor").addClass("bottomUpAnimation")
+      $(".turn").addClass("bottomUpAnimation")
+
+      $(".playAgainBox").removeClass("hidden");
+      $(".turn").addClass("winningBox")
+      $(".playerText").removeClass("mt-[20%]");
+      $(".playerText").addClass("mt-[1.9vh]");
+      if($(".turn").hasClass("redTurnBackground")){
+        nextPlayer = "PLAYER 1";
+        $(".turn").removeClass("redTurnBackground")
+      }
+      else{
+        nextPlayer = "PLAYER 2";
+        $(".turn").removeClass("yellowTurnBackground")
       }
     }
   }
@@ -99,27 +199,28 @@ export const PlayVsPlayer = () => {
     console.log(redTurn)
     console.log("Switching turns")
     timePassed = 0;
-    setTimer(15)
-
-    if(redTurn){
-      console.log("Switching to red")
-      $(".picker").each(function() {
-        $(this).removeClass("yellowPicker")
-        $(this).addClass("redPicker")
-      })
-      $(".turn").removeClass("yellowTurnBackground")
-      $(".turn").addClass("redTurnBackground")
-      setPlayerTurnText("PLAYER 1'S TURN")
-    }
-    else{
-      console.log("Switching to yellow")
-      $(".picker").each(function() {
-        $(this).removeClass("redPicker")
-        $(this).addClass("yellowPicker")
-      })
-      $(".turn").removeClass("redTurnBackground")
-      $('.turn').addClass("yellowTurnBackground")
-      setPlayerTurnText("PLAYER 2'S TURN")
+    setTimer("15")
+    if(!winner){
+      if(redTurn){
+        console.log("Switching to red")
+        $(".picker").each(function() {
+          $(this).removeClass("yellowPicker")
+          $(this).addClass("redPicker")
+        })
+        $(".turn").removeClass("yellowTurnBackground")
+        $(".turn").addClass("redTurnBackground")
+        setPlayerTurnText("PLAYER 1'S TURN")
+      }
+      else{
+        console.log("Switching to yellow")
+        $(".picker").each(function() {
+          $(this).removeClass("redPicker")
+          $(this).addClass("yellowPicker")
+        })
+        $(".turn").removeClass("redTurnBackground")
+        $('.turn').addClass("yellowTurnBackground")
+        setPlayerTurnText("PLAYER 2'S TURN")
+      }
     }
 
 
@@ -131,8 +232,10 @@ export const PlayVsPlayer = () => {
 
 
       setInterval(function() {
-        timePassed += 1;
-        setTimer(MAXTIME - timePassed)
+        if(!winner){
+          timePassed += 1;
+          setTimer("" + ( MAXTIME - timePassed))
+        }
 
         if(timePassed > 15){
           switchTurns();
@@ -150,6 +253,12 @@ export const PlayVsPlayer = () => {
         )
 
         $(this).on("click", function(){
+          if(winner){
+            resetBoard();
+          }
+          if(piecesPlayed == 42){
+            resetBoard();
+          }
           for(let i = 1; i < 7; i++){
             if($(".row-"+i+".col-"+column).hasClass("empty")){
               $(".row-"+i+".col-"+column).removeClass("empty")
@@ -161,8 +270,8 @@ export const PlayVsPlayer = () => {
               }
               checkWin();
               piecesPlayed++;
-
-              switchTurns();
+              if(!winner)
+                switchTurns();
               break;
             }
           }
@@ -204,53 +313,53 @@ export const PlayVsPlayer = () => {
 
             <div role="pieces" className="pieces absolute h-[70.67vh] aspect-[632/636] grid grid-cols-7 grid-rows-6 pb-[17%] pt-[2%] cursor-pointer">
 
-              <div className="row-6 col-1 empty" column="1"></div>
-              <div className="row-6 col-2 empty" column="2"></div>
-              <div className="row-6 col-3 empty" column="3"></div>
-              <div className="row-6 col-4 empty" column="4"></div>
-              <div className="row-6 col-5 empty" column="5"></div>
-              <div className="row-6 col-6 empty" column="6"></div>
-              <div className="row-6 col-7 empty" column="7"></div>
+              <div className="row-6 col-1 empty" column="1"><div className="row6 col1"></div></div>
+              <div className="row-6 col-2 empty" column="2"><div className="row6 col2"></div></div>
+              <div className="row-6 col-4 empty" column="4"><div className="row6 col3"></div></div>
+              <div className="row-6 col-3 empty" column="3"><div className="row6 col4"></div></div>
+              <div className="row-6 col-5 empty" column="5"><div className="row6 col5"></div></div>
+              <div className="row-6 col-6 empty" column="6"><div className="row6 col6"></div></div>
+              <div className="row-6 col-7 empty" column="7"><div className="row6 col7"></div></div>
               
-              <div className="row-5 col-1 empty" column="1"></div>
-              <div className="row-5 col-2 empty" column="2"></div>
-              <div className="row-5 col-3 empty" column="3"></div>
-              <div className="row-5 col-4 empty" column="4"></div>
-              <div className="row-5 col-5 empty" column="5"></div>
-              <div className="row-5 col-6 empty" column="6"></div>
-              <div className="row-5 col-7 empty" column="7"></div>
+              <div className="row-5 col-1 empty" column="1"><div className="row5 col1"></div></div>
+              <div className="row-5 col-2 empty" column="2"><div className="row5 col2"></div></div>
+              <div className="row-5 col-3 empty" column="3"><div className="row5 col3"></div></div>
+              <div className="row-5 col-4 empty" column="4"><div className="row5 col4"></div></div>
+              <div className="row-5 col-5 empty" column="5"><div className="row5 col5"></div></div>
+              <div className="row-5 col-6 empty" column="6"><div className="row5 col6"></div></div>
+              <div className="row-5 col-7 empty" column="7"><div className="row5 col7"></div></div>
               
-              <div className="row-4 col-1 empty" column="1"></div>
-              <div className="row-4 col-2 empty" column="2"></div>
-              <div className="row-4 col-3 empty" column="3"></div>
-              <div className="row-4 col-4 empty" column="4"></div>
-              <div className="row-4 col-5 empty" column="5"></div>
-              <div className="row-4 col-6 empty" column="6"></div>
-              <div className="row-4 col-7 empty" column="7"></div>
+              <div className="row-4 col-1 empty" column="1"><div className="row4 col1"></div></div>
+              <div className="row-4 col-2 empty" column="2"><div className="row4 col2"></div></div>
+              <div className="row-4 col-3 empty" column="3"><div className="row4 col3"></div></div>
+              <div className="row-4 col-4 empty" column="4"><div className="row4 col4"></div></div>
+              <div className="row-4 col-5 empty" column="5"><div className="row4 col5"></div></div>
+              <div className="row-4 col-6 empty" column="6"><div className="row4 col6"></div></div>
+              <div className="row-4 col-7 empty" column="7"><div className="row4 col7"></div></div>
               
-              <div className="row-3 col-1 empty" column="1"></div>
-              <div className="row-3 col-2 empty" column="2"></div>
-              <div className="row-3 col-3 empty" column="3"></div>
-              <div className="row-3 col-4 empty" column="4"></div>
-              <div className="row-3 col-5 empty" column="5"></div>
-              <div className="row-3 col-6 empty" column="6"></div>
-              <div className="row-3 col-7 empty" column="7"></div>
+              <div className="row-3 col-1 empty" column="1"><div className="row3 col1"></div></div>
+              <div className="row-3 col-2 empty" column="2"><div className="row3 col2"></div></div>
+              <div className="row-3 col-3 empty" column="3"><div className="row3 col3"></div></div>
+              <div className="row-3 col-4 empty" column="4"><div className="row3 col4"></div></div>
+              <div className="row-3 col-5 empty" column="5"><div className="row3 col5"></div></div>
+              <div className="row-3 col-6 empty" column="6"><div className="row3 col6"></div></div>
+              <div className="row-3 col-7 empty" column="7"><div className="row3 col7"></div></div>
               
-              <div className="row-2 col-1 empty" column="1"></div>
-              <div className="row-2 col-2 empty" column="2"></div>
-              <div className="row-2 col-3 empty" column="3"></div>
-              <div className="row-2 col-4 empty" column="4"></div>
-              <div className="row-2 col-5 empty" column="5"></div>
-              <div className="row-2 col-6 empty" column="6"></div>
-              <div className="row-2 col-7 empty" column="7"></div>
+              <div className="row-2 col-1 empty" column="1"><div className="row2 col1"></div></div>
+              <div className="row-2 col-2 empty" column="2"><div className="row2 col2"></div></div>
+              <div className="row-2 col-3 empty" column="3"><div className="row2 col3"></div></div>
+              <div className="row-2 col-4 empty" column="4"><div className="row2 col4"></div></div>
+              <div className="row-2 col-5 empty" column="5"><div className="row2 col5"></div></div>
+              <div className="row-2 col-6 empty" column="6"><div className="row2 col6"></div></div>
+              <div className="row-2 col-7 empty" column="7"><div className="row2 col7"></div></div>
               
-              <div className="row-1 col-1 empty" column="1"></div>
-              <div className="row-1 col-2 empty" column="2"></div>
-              <div className="row-1 col-3 empty" column="3"></div>
-              <div className="row-1 col-4 empty" column="4"></div>
-              <div className="row-1 col-5 empty" column="5"></div>
-              <div className="row-1 col-6 empty" column="6"></div>
-              <div className="row-1 col-7 empty" column="7"></div>
+              <div className="row-1 col-1 empty" column="1"><div className="row1 col1"></div></div>
+              <div className="row-1 col-2 empty" column="2"><div className="row1 col2"></div></div>
+              <div className="row-1 col-3 empty" column="3"><div className="row1 col3"></div></div>
+              <div className="row-1 col-4 empty" column="4"><div className="row1 col4"></div></div>
+              <div className="row-1 col-5 empty" column="5"><div className="row1 col5"></div></div>
+              <div className="row-1 col-6 empty" column="6"><div className="row1 col6"></div></div>
+              <div className="row-1 col-7 empty" column="7"><div className="row1 col7"></div></div>
             </div>
 
             <div role="Frontpieces" className="frontPieces absolute h-[70.67vh] aspect-[632/636] grid grid-cols-7 pb-[17%] pt-[2%] cursor-pointer z-40">
@@ -264,11 +373,13 @@ export const PlayVsPlayer = () => {
             </div>
 
 
-
-              <div className ="turn redTurnBackground image h-[16.67vh] absolute bottom-[-10%] w-[30%] ml-[35%] flex flex-col z-30">
-                <p className='text-HeadingXs mt-[20%] text-center'>{playerTurnText}</p>
+            <div className="flex justify-center turnWrapper absolute bottom-[-10%] w-[100%]">
+              <div className ="turn align-top redTurnBackground image h-[16.67vh] w-[30%] flex flex-col z-30">
+                <p className='text-HeadingXs mt-[20%] text-center playerText'>{playerTurnText}</p>
                 <p className="text-center text-HeadingLg">{timer}</p>
+                <button className="playAgainBox hidden rounded-[5000px] w-[14.4vh] bg-darkPurple text-white py-2 px-2 text-center m-auto text-HeadingXs">PLAY AGAIN</button>
               </div>
+            </div>
 
               <div className="hidden picker picker1 redPicker image absolute h-[3.3vh] top-[-3%] ml-[4vh] aspect-square z-30">
               </div>
@@ -306,7 +417,7 @@ export const PlayVsPlayer = () => {
           </div>
         </div>
 
-          <div className="w-[100vw] h-[22.2vh] absolute bottom-0 bg-darkPurple z-[0]"></div>
+          <div className="bottomColor w-[100vw] h-[22.2vh] absolute bottom-0 bg-darkPurple z-[0] rounded-t-[40px]"></div>
     </div>
   )
 }
